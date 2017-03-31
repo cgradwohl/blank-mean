4a6626193c91dc223ca28dd8aa7eb1326b736c85
Chris Gradwohl <christophergradwohl@gmail.com>
2017-03-31 01:21:01 -0700

    added server to serve angular2 cli starter code

---
+++ server.js
@@ -0,0 +1,49 @@
+// sets up a simple express app, with an /api route and all
+// other routes are directed towards the dist/index.html page.
+// This catch all route, denoted with *, MUST come last after
+// all other API routes have been defined.
+
+
+
+
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+// Get our API routes
+const api = require('./server/routes/api');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
